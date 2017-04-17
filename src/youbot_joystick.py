a0efe7170fc3f68286de2fb1269f2d0187822d19
joycexx94 <xx94@cornell.edu>
2017-04-17 11:10:16 -0400

    include y-axis

--- src/youbot_joystick.py
+++ src/youbot_joystick.py
@@ -7,13 +7,14 @@ import std_srvs.srv
 class TeleopTurtle:
     def __init__(self):
         rospy.init_node('turtle_teleop_joy')
-        self.linear_axis = rospy.get_param("axis_linear")
+        self.linear_axis_x = rospy.get_param("axis_linear_x")
+        self.linear_axis_y = rospy.get_param("axis_linear_y")
         self.angular_axis = rospy.get_param("axis_angular")
         self.linear_scale = rospy.get_param("scale_linear")
         self.angular_scale = rospy.get_param("scale_angular")
         self.robot_topic_name = rospy.get_param("robot_topic_name") 
 
-        rospy.wait_for_service('reset')
+        #rospy.wait_for_service('reset')
 
         self.twist = None
         self.twist_pub = rospy.Publisher(self.robot_topic_name, Twist,queue_size=1)
@@ -27,12 +28,17 @@ class TeleopTurtle:
 
     def callback(self,msg):
         twist = Twist()
-        twist.linear.x = 5*abs(msg.axes[3]) * msg.axes[self.linear_axis]
+        twist.linear.x = linear_scale + 0.2*msg.axes[3] * msg.axes[self.linear_axis_x]
+        twist.linear.y = linear_scale + 0.2*msg.axes[3] * msg.axes[self.linear_axis_y]
         twist.angular.z = self.angular_scale * msg.axes[self.angular_axis]
         self.twist=twist
 	if (msg.buttons[3]==1 and msg.buttons[2]==1):
-          serv = rospy.ServiceProxy('reset', std_srvs.srv.Empty)
-          serv()
+          twist.linear.x = 0
+          twist.linear.y = 0
+          twist.angular.z = 0
+          #serv = rospy.ServiceProxy('reset', std_srvs.srv.Empty)
+          #serv()
+
 
 if __name__ == "__main__": 
     TeleopTurtle()
