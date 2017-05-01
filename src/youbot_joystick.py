7d1c2571e33341732a09737075d5110914b89785
joycexx94 <xx94@cornell.edu>
2017-05-01 15:34:06 -0400

    modified e-stop

--- src/youbot_joystick.py
+++ src/youbot_joystick.py
@@ -28,6 +28,16 @@ class TeleopTurtle:
             if self.twist:
                 self.twist_pub.publish(self.twist)
 
+    def switch_off_motors(self):
+         rospy.wait_for_service('/base/switchOffMotors')
+         switch_off = rospy.ServiceProxy('/base/switchOffMotors', std_srvs.srv.Empty)
+         switch_off()
+
+    def switch_on_motors(self):
+         rospy.wait_for_service('/base/switchOnMotors')
+         switch_on = rospy.ServiceProxy('/base/switchOnMotors', std_srvs.srv.Empty)
+         switch_on()
+
     def callback(self,msg):
         twist = Twist()
         twist.linear.x = (self.linear_scale + 1.9*msg.axes[3]) * msg.axes[self.linear_axis_x]
@@ -35,12 +45,16 @@ class TeleopTurtle:
         twist.angular.z = self.angular_scale * msg.axes[self.angular_axis]
         self.twist=twist
 	if (msg.buttons[3]==1 and msg.buttons[2]==1):
-          twist.linear.x = 0
-          twist.linear.y = 0
-          twist.angular.z = 0
+          #twist.linear.x = 0
+          #twist.linear.y = 0
+          #twist.angular.z = 0
           #kill_base = rospy.ServiceProxy('base/switchOffMotors', std_srvs.srv.Empty)
           #kill_base()
-
+          self.switch_off_motors()
+        if (msg.buttons[0]==1 and msg.buttons[1]==1):
+          #turn_on_base = rospy.ServiceProxy('base/switchOnMotors', std_srvs.srv.Empty)
+          #turn_on_base()
+          self.switch_on_motors()
 
 if __name__ == "__main__": 
     TeleopTurtle()
